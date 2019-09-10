ec8d33122f01330138b466b82a50505594734547
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-09-11 00:53:11 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM alpine:latest
+MAINTAINER boredazfcuk
+ENV REPO="sherpya/geolite2legacy" \
+   DBURL="https://geolite.maxmind.com/download/geoip/database" \
+   APPBASE="/GeoLite2Legacy" \
+   DBDIR="/usr/share/GeoIP" \
+   APPDEPENDENCIES="git curl python py-ipaddr tzdata"
+
+COPY update-geoip.sh /usr/local/bin/update-geoip.sh
+
+RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD STARTED *****" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Add group, user and required directories" && \
+   mkdir -p "${APPBASE}" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install application dependencies" && \
+   apk add --no-cache --no-progress ${APPDEPENDENCIES}  && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Installing ${REPO}" && \
+   git clone -b master "https://github.com/${REPO}.git" "${APPBASE}" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Set launch script permissions" && \
+   chmod +x "/usr/local/bin/update-geoip.sh" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD COMPLETE *****"
+
+HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
+   CMD (if [ $(find "${DBDIR}" -type f -name "Geo*.dat" | wc -l) -ne 3 ] || [ $(find "${DBDIR}" -type f -name "Geo*.dat" -mtime +5 | wc -l) -ne 0 ]; then exit 1; fi)
+
+VOLUME "${DBDIR}"
+WORKDIR "${APPBASE}"
+
+CMD /usr/local/bin/update-geoip.sh && /usr/sbin/crond -f -L /dev/stdout
---
+++ update-geoip.sh
@@ -0,0 +1,70 @@
+#!/bin/ash
+
+Initialise(){
+   echo "$(date '+%Y-%m-%d %H:%M:%S') | ***** Starting GeoIPDb container using sherpya's geolite2legacy to convert to the legacy format *****"
+
+   if [ ! -e "${DBDIR}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Database directory does not exist, creating ${DBDIR}"; mkdir -p "${DBDIR}"; fi
+
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    GeoLite2Legacy directory: ${APPDATA}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    GeoIP Database directory: ${DBDIR}"
+
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Correct owner and group of application and database directories, if required"
+   find "${DBDIR}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${DBDIR}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+   find "${APPBASE}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${APPBASE}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+
+   if [ $(grep -c "update-geoip" /var/spool/root) -lt 1 ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Initialise crontab"
+      MIN=$(((RANDOM%60)))
+      echo -e "SHELL=/bin/ash\n\n# m h  dom mon dow   command\n${MIN} 5 * * 4 /usr/local/bin/update-geoip.sh" > /tmp/crontab.tmp
+      crontab /tmp/crontab.tmp
+      rm /tmp/crontab.tmp
+   }
+}
+
+GeoLite2Legacy(){
+   if [ ! -e "${APPBASE}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Installing ${REPO}"
+      mkdir -p "${APPBASE}"
+      git clone -b master "https://github.com/${REPO}.git" "${APPBASE}"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Checking ${REPO}... "
+      cd "${APPBASE}" || exit 1
+      git pull
+      cd / || exit 1
+   fi
+}
+
+UpdateDatabases(){
+   local ZIPTEMP=$(mktemp -d)
+   local DBTEMP=$(mktemp -d)
+   if [ -z "$(find "${DBDIR}" -type f -name 'GeoIP.dat')" ] || [ "$(find "${DBDIR}" -type f -name 'GeoIP.dat' -mtime +5 | wc -l)" -ne 0 ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Install GeoIP Country database"
+      if [ -e "${DBDIR}/GeoIP.dat" ]; then rm "${DBDIR}/GeoIP.dat"; fi
+      curl -sS -L "${DBURL}/GeoLite2-Country-CSV.zip" > "${ZIPTEMP}/GeoLite2-Country-CSV.zip"
+      python "${APPBASE}/geolite2legacy.py" -i "${ZIPTEMP}/GeoLite2-Country-CSV.zip" -f "${APPBASE}/geoname2fips.csv" -o "${DBTEMP}/GeoIP.dat"
+      mv -f "${DBTEMP}/GeoIP.dat" "${DBDIR}"
+   fi
+   if [ -z "$(find "${DBDIR}" -type f -name 'GeoLiteCity.dat')" ] || [ "$(find "${DBDIR}" -type f -name 'GeoLiteCity.dat' -mtime +5 | wc -l)" -ne 0 ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Install GeoIP City database"
+      if [ -e "${DBDIR}/GeoLiteCity.dat" ]; then rm "${DBDIR}/GeoLiteCity.dat"; fi
+      curl -sS -L "${DBURL}/GeoLite2-City-CSV.zip" > "${ZIPTEMP}/GeoLite2-City-CSV.zip"
+      python "${APPBASE}/geolite2legacy.py" -i "${ZIPTEMP}/GeoLite2-City-CSV.zip" -f "${APPBASE}/geoname2fips.csv" -o "${DBTEMP}/GeoLiteCity.dat"
+      mv -f "${DBTEMP}/GeoLiteCity.dat" "${DBDIR}"
+   fi
+   if [ -z "$(find "${DBDIR}" -type f -name 'GeoIPASNum.dat')" ] || [ "$(find "${DBDIR}" -type f -name 'GeoIPASNum.dat' -mtime +5 | wc -l)" -ne 0 ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Install GeoIP ASNum database"
+      if [ -e "${DBDIR}/GeoIPASNum.dat" ]; then rm "${DBDIR}/GeoIPASNum.dat"; fi
+      curl -sS -L "${DBURL}/GeoLite2-ASN-CSV.zip" > "${ZIPTEMP}/GeoLite2-ASN-CSV.zip"
+      python "${APPBASE}/geolite2legacy.py" -i "${ZIPTEMP}/GeoLite2-ASN-CSV.zip" -o "${DBTEMP}/GeoIPASNum.dat"
+      mv -f "${DBTEMP}/GeoIPASNum.dat" "${DBDIR}"
+   fi
+   rm -fr "${ZIPTEMP}" "${DBTEMP}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** GeoIP Database Update Complete *****"
+}
+
+##### Script #####
+Initialise
+GeoLite2Legacy
+UpdateDatabases
