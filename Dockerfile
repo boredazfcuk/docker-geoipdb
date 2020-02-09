341f88b2062c2a3998d70da97c2ec97df62774a3
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-09 23:06:49 +0000

    Should be a change log. If not, I've made so many updates I've forgotten, or too few to bother updating it

--- Dockerfile
+++ Dockerfile
@@ -11,11 +11,11 @@ echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install application dependencies" && \
    apk add --no-cache --no-progress ${app_dependencies} && \
    ln -s /usr/bin/python3 /usr/bin/python
 
-COPY update-geoip.sh /usr/local/bin/update-geoip.sh
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 COPY healthcheck.sh /usr/local/bin/healthcheck.sh
 
 RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | Set launch script permissions" && \
-   chmod +x "/usr/local/bin/update-geoip.sh" "/usr/local/bin/healthcheck.sh" && \
+   chmod +x "/usr/local/bin/entrypoint.sh" "/usr/local/bin/healthcheck.sh" && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD COMPLETE *****"
 
 HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
@@ -24,4 +24,4 @@ HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
 VOLUME "${geoip_db_dir}"
 WORKDIR "${app_base_dir}"
 
-CMD /usr/local/bin/update-geoip.sh && /usr/sbin/crond -f -L /dev/stdout
+ENTRYPOINT /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/ash
 
 Initialise(){
+   if [ "${#}" -eq 1 ] && [ "${1}" = "--update-only" ]; then update_only="True"; fi
    app_repo="sherpya/geolite2legacy"
    geoip_db_url="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=${maxmind_licence_key}&suffix=zip"
-
    echo -e "\n"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Starting GeoIPDb container using sherpya's geolite2legacy to convert to the legacy format *****"
-
    if [ ! -e "${geoip_db_dir}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Database directory does not exist, creating ${geoip_db_dir}"; mkdir -p "${geoip_db_dir}"; fi
    if [ -z "${maxmind_licence_key}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR:   Maxmind licence key not specified. Cannot continue - Exiting"; sleep 60; exit 1; fi
-
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    GeoLite2Legacy directory: ${app_base_dir}"
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    GeoIP Database directory: ${geoip_db_dir}"
-
-   if [ "$(grep -c 'update-geoip.sh' /etc/crontabs/root)" -lt 1 ]; then
+   if [ "$(grep -c 'entrypoint.sh' /etc/crontabs/root)" -lt 1 ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Initialise crontab"
       minute=$(((RANDOM%60)))
-      echo -e "# min   hour    day     month   weekday command\n${minute} 5 5 * 4 /usr/local/bin/update-geoip.sh >/dev/stdout 2>&1" > /tmp/crontab.tmp
+      {
+         echo "# min   hour    day     month   weekday command"
+         echo "${minute} 5 5 * 4 /usr/local/bin/entrypoint.sh --update-only >/dev/stdout 2>&1"
+      } > /tmp/crontab.tmp
       crontab /tmp/crontab.tmp
       rm /tmp/crontab.tmp
    fi
@@ -47,10 +47,18 @@ UpdateDatabase(){
       mv -f "${db_temp_dir}/GeoIP.dat" "${geoip_db_dir}"
       rm -fr "${zip_temp_dir}" "${db_temp_dir}"
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    GeoIP Country database installation complete"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    GeoIP Country database installed and up-to-date"
    fi
+}
+
+LaunchCrontab(){
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Starting crontab"
+   exec /usr/sbin/crond -f -L /dev/stdout
 }
 
 ##### Script #####
 Initialise
 GeoLite2Legacy
 UpdateDatabase
+if [ -z "${update_only}" ]; then LaunchCrontab; fi
